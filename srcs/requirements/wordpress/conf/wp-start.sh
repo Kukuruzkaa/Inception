83883cffbd13cb754d35ebd2de51dc46ef460c7f
ddiakova <ddiakova@student.42.fr>
2022-11-21 20:07:08 +0100

    wp built

--- srcs/requirements/wordpress/conf/wp-start.sh
+++ srcs/requirements/wordpress/conf/wp-start.sh
@@ -8,6 +8,7 @@
 set -xv
 
 sleep 5
+wp download core --allow-root
 wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --prompt=${DB_PASS} --dbhost=mariadb --allow-root
 wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} \
     --admin_email=${WP_ADMIN_EMAIL} --prompt=${WP_PASS} 
