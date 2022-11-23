4ca648e82250e9835a8507903df750a9c252e1e9
Daria DIAKOVA <dariadiakova@gmail.com>
2022-11-23 16:57:26 +0100

    wp docker modified

--- srcs/requirements/wordpress/conf/wp-start.sh
+++ srcs/requirements/wordpress/conf/wp-start.sh
@@ -1,16 +1,35 @@
 #!bin/sh
 
-if [ ! -f "/var/www/wp-config.php" ]; then
+if [ ! -f "/var/www/wp-config.php" ]; 
+then
 
-set -xv
+    set -xv
 
-sleep 5
-wp download core --allow-root
-wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --prompt=${DB_PASS} --dbhost=mariadb --allow-root
-wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} \
-    --admin_email=${WP_ADMIN_EMAIL} --prompt=${WP_PASS} 
-wp user create --user=${WP_USER} --admin_email=${WP_USER_EMAIL} --prompt=${WP_PASS} 
+    sleep 5
+    cd /var/www/html
+    wp download core --allow-root
+    wp config create \
+            --dbname=${DB_NAME} \
+            --dbuser=${DB_USER} \
+            --prompt=${DB_PASS} \
+            --dbcharset="utf8" \
+            --dbhost=mariadb:3306 \
+            --allow-root
+            
+    wp core install \
+            --url=${DOMAIN_NAME} \
+            --title=${WP_TITLE} \
+            --admin_user=${WP_ADMIN_USER} \
+            --prompt=${WP_PASS}
+            --admin_email=${WP_ADMIN_EMAIL} 
+            --allow-root
+
+    wp user create \
+            --user=${WP_USER} \
+            --user_email=${WP_EMAIL} \
+            --prompt=${WP_PASS} \
+            --allow-root
+    chown -R www-data:www-data /var/www/html/
 fi
-
 
 exec php-fpm8 -F
